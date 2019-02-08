a92d379460715e172e577c2bd3f89c60287986dc
hukim89 <uk0662@gmail.com>
2019-02-08 17:20:03 +0900

    111

--- spring-basis-security-basis/src/main/java/com/wan/basis/controller/SecurityController.java
+++ spring-basis-security-basis/src/main/java/com/wan/basis/controller/SecurityController.java
@@ -23,7 +23,7 @@ public class SecurityController {
 	public String test(Locale locale, Model model) {
 		
 		logger.info("Welcome home! The client locale is {}.", locale);
-
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
